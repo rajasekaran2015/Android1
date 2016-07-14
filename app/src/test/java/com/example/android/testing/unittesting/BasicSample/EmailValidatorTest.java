c7ecdcc1f7dc74d9f0cb33f9533a91fe2bf1aa35
Debrup-nitdgp <debrup_uit@yahoo.com>
2016-07-14 18:12:14 +0530

    Commit

---
+++ app/src/test/java/com/example/android/testing/unittesting/BasicSample/EmailValidatorTest.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2015, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.testing.unittesting.BasicSample;
+
+import android.test.suitebuilder.annotation.SmallTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.junit.Assert.assertTrue;
+
+@Config(constants = BuildConfig.class)
+@RunWith(RobolectricGradleTestRunner.class)
+/**
+ * Unit tests for the EmailValidator logic.
+ */
+@SmallTest
+public class EmailValidatorTest {
+
+   // private static final String TEST_NAME = "name@email.com";
+
+    //private static final String TEST_EMAIL = "name@email.co.uk";
+
+
+    @Test
+    public void emailValidator_CorrectEmailSimple_ReturnsTrue() {
+        assertTrue(EmailValidator.isValidEmail("name@email.com"));
+    }
+
+    @Test
+    public void emailValidator_CorrectEmailSubDomain_ReturnsTrue() {
+        assertTrue(EmailValidator.isValidEmail("name@email.co.uk"));
+    }
+
+    //@Test
+   // public void emailValidator_InvalidEmailNoTld_ReturnsFalse() {
+       // assertFalse(EmailValidator.isValidEmail("name@email"));
+    //}
+
+   // @Test
+   // public void emailValidator_InvalidEmailDoubleDot_ReturnsFalse() {
+       // assertFalse(EmailValidator.isValidEmail("name@email..com"));
+   // }
+
+    //@Test
+    //public void emailValidator_InvalidEmailNoUsername_ReturnsFalse() {
+       // assertFalse(EmailValidator.isValidEmail("@email.com"));
+    //}
+
+   // @Test
+   // public void emailValidator_EmptyString_ReturnsFalse() {
+       // assertFalse(EmailValidator.isValidEmail(""));
+    //}
+
+    //@Test
+    //public void emailValidator_NullEmail_ReturnsFalse() {
+       // assertFalse(EmailValidator.isValidEmail(null));
+    //}
+}
