efc40543c5a574de985dcc75231d63c90f6ec28c
Debrup-nitdgp <debrup_uit@yahoo.com>
2016-07-15 18:22:26 +0530

    Commit

--- app/src/test/java/com/example/android/testing/unittesting/BasicSample/EmailValidatorTest.java
+++ app/src/test/java/com/example/android/testing/unittesting/BasicSample/EmailValidatorTest.java
@@ -32,10 +32,10 @@ import static org.junit.Assert.assertTrue;
 public class EmailValidatorTest {
 
 
-   // @Test
-    //public void emailValidator_CorrectEmailSimple_ReturnsTrue() {
-    //  assertTrue(EmailValidator.isValidEmail("name@email.com"));
-   // }
+  @Test
+    public void emailValidator_CorrectEmailSimple_ReturnsTrue() {
+      assertTrue(EmailValidator.isValidEmail("name@email.com"));
+  }
 
     @Test
     public void emailValidator_CorrectEmailSubDomain_ReturnsTrue() {
